a28cd6d4065a1f4e89c25165ed61d6f148fad5ce
Pureum309 <psun27@my.bcit.ca>
2022-02-25 00:25:46 -0800

    initial commit

---
+++ comps/icon.js
@@ -0,0 +1,42 @@
+//MUST HAVE - CREATE A TEMPLATE TAG
+var template_icons = document.createElement("template"); //<template> </template> RULE
+
+//To-do - CREATE THE UI HERE!
+template_icons.innerHTML = `
+<style>
+    #item_cont {
+
+    }
+</style>
+
+<div id='item_cont'>
+    <div id='item_text'>
+    <img src='./imgs/reduce.png' /> text
+    </div>
+</div>
+`;
+
+//MUST HAVE - CREATE A CLASS WITH HTMLELEMENT POWERS (interfaces/functionalities)
+class TheItems extends HTMLElement {
+
+    //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
+    constructor(){
+        super(); //pass on the HTMLElement super powers!
+        this.attachShadow({mode:"open"}) //Attach it to the shadowRoot
+
+        //To-do - CREATE THE STATES FOR THE UI HERE!
+    }
+
+    //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
+    connectedCallback(){
+        this.shadowRoot.appendChild(template_icons.content.cloneNode(true)); //use the template to make a clone
+        if(this.getAttribute("item_text")) {
+            this.shadowRoot.querySelector("#item_text").innerText = this.getAttribute("item_text");
+        }
+    }
+
+    //To-do - CREATE THE FUNCTIONALITIES HERE!
+}
+
+//MUST HAVE - define the tag for the custom elements
+customElements.define("item-comp", TheItems)
