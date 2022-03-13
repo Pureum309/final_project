f4979a803e1ab6477466c857b47a3be4f43d7460
Pureum309 <psun27@my.bcit.ca>
2022-03-13 01:01:16 -0800

    finalized link page, add CTA button

--- comps/logo.js
+++ comps/logo.js
@@ -10,11 +10,33 @@ template_logo.innerHTML = `
     flex-wrap: wrap;
     align-items: center;
     justify-content: center;
+    font-size: 1.25em;
+    padding: 5em;
     }
+
 img {
     width: 50%;
     height: 100%;
     padding: 1em;
+}
+
+a {
+    text-decoration: none;
+    border-radius: 6px;
+    padding: 10px 30px;
+    text-align: center;
+    background-color: #549DE3;
+    color: white;
+    
+}
+
+a:hover {
+    background-color:#4095c6;
+    color: #16314b;
+}
+
+#desc_text {
+    padding-bottom: 2em;
 }
 
 </style>
@@ -24,6 +46,7 @@ img {
     <div id = desc_text>
     Lorem ipsumdlfksjfleifjslfjdk
     </div>
+    <a href="url">Join Now</a>
 </div>
 `;
 
@@ -42,12 +65,11 @@ class TheLogo extends HTMLElement {
     //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
     connectedCallback(){
         this.shadowRoot.appendChild(template_logo.content.cloneNode(true)); //use the template to make a clone
-        if(this.getAttribute("desc_text")){
             this.shadowRoot.querySelector('#desc_text').innerText = this.getAttribute('desc_text')
-        }
-        if(this.getAttribute("logo_name")) {
             this.shadowRoot.querySelector("#item_image > img").src = "./graphics/" + this.getAttribute("logo_name");
-        }
+            this.shadowRoot.querySelector("#item_image > a").href = "https://www." + this.getAttribute("link_name");
+        
+
     }
 
 
